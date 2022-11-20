65ba4f2b50272cc17e430993ae309a48381ef822
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-11-20 21:04:09 +0700

    update

--- js/app.js
+++ js/app.js
@@ -123,10 +123,36 @@ $("#submit-business").click(function(){
   let checked = $("input[name$='matrix-1']:checked");
   let checkedVal = checked.val();
   // var val_date = $(".matrix-1").val()
+  if (checkedVal == undefined) {
+    $(".matrix-1").parent().parent().next().addClass('show').text('Please select hour')
+    // $("#10").addClass("iaia")
+  } else {
+    $(".matrix-1").parent().parent().next().removeClass('show')
+  }
 
-  var exhibitor = $("#exhibitor_product").val();
+  console.log(checkedVal == undefined);
+
+    
 
   var date_ = $("#select-date").val()
+    if (date_.length < 1) {
+      $("#select-date").next().next().addClass('show').text('Please Input Date');
+      $("#select-date").focus()
+  } else {
+      $("#select-date").removeClass('show');
+  }
 
-  alert(date_)
+  var exhibitor = $("#exhibitor_product").val();
+    if (exhibitor.length < 1) {
+        $("#exhibitor_product").next().next().next().addClass('show').text('Please select Exhibitor');
+        $("#exhibitor_product").focus()
+    } else {
+        $("#exhibitor_product").next().next().next().removeClass('show');
+    }
+
+// console.log(date_.length < 1);
+    if (exhibitor.length > 0 && date_.length > 0 && checkedVal !=undefined) {
+      alert("Berhasil")
+    }
+  
 })
