66b080234e89e33576f21e5ee2e49dd008c4e26f
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-12-10 14:47:40 +0700

    test

--- js/auth-login.js
+++ js/auth-login.js
@@ -18,3 +18,22 @@
         alert("email atau password salah")
     }
   })
+
+
+  function onSubmit(token) {
+    alert('thanks ' + document.getElementById('field').value);
+  }
+
+  function validate(event) {
+    event.preventDefault();
+    if (!document.getElementById('field').value) {
+      alert("You must add text to the required field");
+    } else {
+      grecaptcha.execute();
+    }
+  }
+
+  function onload() {
+    var element = document.getElementById('submit');
+    element.onclick = validate;
+  }
